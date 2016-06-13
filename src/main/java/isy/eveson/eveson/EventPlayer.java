2c0da3993dc08530a81b40127694aaec54606f07
Ming Gao <mgao@techfak.uni-bielefeld.de>
2016-06-13 15:05:34 +0200

    JPAudioResourceFolder

--- src/main/java/isy/eveson/eveson/EventPlayer.java
+++ src/main/java/isy/eveson/eveson/EventPlayer.java
@@ -10,10 +10,12 @@ import com.jsyn.Synthesizer;
 import com.jsyn.unitgen.LineOut;
 import static isy.eveson.eveson.ScopePlayer.Type.ADJUST;
 import static isy.eveson.eveson.ScopePlayer.Type.PLAY;
+import isy.eveson.eveson.jp.JPAudioResoureFolder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.dc.jps.core.JPService;
 import rsb.RSBException;
 
 /**
@@ -42,6 +44,15 @@ public class EventPlayer {
     }
 
     public static void main(String args[]) throws Throwable {
+        
+        // JPS setup
+        JPService.setApplicationName("eveson");
+        JPService.registerProperty(JPAudioResoureFolder.class);
+        JPService.parseAndExitOnError(args);
+        
+        System.out.println("audio folder: "+JPService.getProperty(JPAudioResoureFolder.class).getValue().getAbsolutePath());
+        String prefix = JPService.getProperty(JPAudioResoureFolder.class).getValue().getAbsolutePath();
+        
         synth = JSyn.createSynthesizer();
         lineOut = new LineOut();
         synth.add(lineOut);
@@ -49,16 +60,16 @@ public class EventPlayer {
         lineOut.start();
         
         Map<String, ScopePlayer> scopeSampleMap = new HashMap<>();
-        scopeSampleMap.put("/home/kitchen/floor/", new ScopePlayer("/samples/purr.wav",ADJUST));
-        scopeSampleMap.put("/home/living/motionsensor/", new ScopePlayer("/samples/bird/swiftoid.wav",PLAY));
-        scopeSampleMap.put("/home/kitchen/motionsensor/", new ScopePlayer("/samples/bird/inspectorj.wav",PLAY));
-        scopeSampleMap.put("/home/sports/motionsensor/", new ScopePlayer("/samples/bird/cajo.wav",PLAY));
-        scopeSampleMap.put("/home/living/ambientlight/", new ScopePlayer("/samples/sound_beim_anzuenden.wav",PLAY));
-        scopeSampleMap.put("/home/kitchen/ambientlight/", new ScopePlayer("/samples/sound_beim_anzuenden.wav",PLAY));
-        scopeSampleMap.put("/home/living/temperaturesensor", new ScopePlayer("/samples/wind.wav",ADJUST));
-        scopeSampleMap.put("/home/kitchen/powerconsumptionsensor", new ScopePlayer("/samples/rain.wav",ADJUST));
-        scopeSampleMap.put("/home/living/powerconsumptionsensor", new ScopePlayer("/samples/rain.wav",ADJUST));
-        scopeSampleMap.put("/home/kitchen/soundlocation", new ScopePlayer("/samples/woodpecker.wav",PLAY));
+        scopeSampleMap.put("/home/kitchen/floor/", new ScopePlayer(prefix+"purr.wav",ADJUST));
+        scopeSampleMap.put("/home/living/motionsensor/", new ScopePlayer(prefix+"bird/swiftoid.wav",PLAY));
+        scopeSampleMap.put("/home/kitchen/motionsensor/", new ScopePlayer("bird/inspectorj.wav",PLAY));
+        scopeSampleMap.put("/home/sports/motionsensor/", new ScopePlayer("bird/cajo.wav",PLAY));
+        scopeSampleMap.put("/home/living/ambientlight/", new ScopePlayer("sound_beim_anzuenden.wav",PLAY));
+        scopeSampleMap.put("/home/kitchen/ambientlight/", new ScopePlayer("sound_beim_anzuenden.wav",PLAY));
+        scopeSampleMap.put("/home/living/temperaturesensor", new ScopePlayer("wind.wav",ADJUST));
+        scopeSampleMap.put("/home/kitchen/powerconsumptionsensor", new ScopePlayer("rain.wav",ADJUST));
+        scopeSampleMap.put("/home/living/powerconsumptionsensor", new ScopePlayer("rain.wav",ADJUST));
+        scopeSampleMap.put("/home/kitchen/soundlocation", new ScopePlayer("woodpecker.wav",PLAY));
 //        scopeSampleMap.put("/home/<location>/<microphon>/audio/", "Brightness.wav");
 //        scopeSampleMap.put("/apartment/calendar/", "Brightness.wav");
 
---
+++ src/main/java/isy/eveson/eveson/jp/JPAudioResoureFolder.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package isy.eveson.eveson.jp;
+
+import java.io.File;
+import org.dc.jps.core.JPService;
+import org.dc.jps.exception.JPNotAvailableException;
+import org.dc.jps.preset.AbstractJPDirectory;
+import org.dc.jps.preset.JPPrefix;
+import org.dc.jps.tools.FileHandler;
+
+/**
+ *
+ * @author mgao
+ */
+public class JPAudioResoureFolder extends AbstractJPDirectory {
+
+    public final static String[] COMMAND_IDENTIFIERS = {"-r", "--audio-resource-folder"};
+    
+    public JPAudioResoureFolder() {
+        super(COMMAND_IDENTIFIERS, FileHandler.ExistenceHandling.Must, FileHandler.AutoMode.Off);
+    }
+    
+    @Override
+    protected File getPropertyDefaultValue() throws JPNotAvailableException {
+        return new File(JPService.getProperty(JPPrefix.class).getValue(), "/share/audio/samples/eveson");
+    }
+
+    @Override
+    public String getDescription() {
+        return "This property configure the audio resource folder.";
+    }
+    
+}
