a50db71d60f32bd2380d897c03f9a5740160cbb4
Ming Gao <mgao@techfak.uni-bielefeld.de>
2016-07-22 12:26:07 +0200

    exp moving average

--- src/main/java/de/citec/csra/remotes/LocationObserver.java
+++ src/main/java/de/citec/csra/remotes/LocationObserver.java
@@ -1,7 +1,6 @@
 package de.citec.csra.remotes;
 
 import de.citec.csra.EventPlayer;
-import de.citec.csra.EvesonConfig;
 import de.citec.csra.ScopePlayer;
 import org.openbase.jul.exception.InstantiationException;
 import org.openbase.jul.pattern.Observable;
@@ -17,9 +16,12 @@ public class LocationObserver implements Observer<LocationData> {
     private static double THRESHOLD_NORMAL;
     private static double THRESHOLD_HIGH;
     private static double THRESHOLD_EXTREME;
-    ScopePlayer sp_normal;
-    ScopePlayer sp_high;
-    ScopePlayer sp_extreme;
+    private ScopePlayer sp_normal;
+    private ScopePlayer sp_high;
+    private ScopePlayer sp_extreme;
+    // parameter and last value for exponential average
+    private double alpha = 0.5;
+    private double lastValue = 0;
 
     public LocationObserver() throws InstantiationException {
         sp_normal = new ScopePlayer(EventPlayer.getInstance().getScopeSampleMap().get("POWER_NORMAL").getSampleFile(), ScopePlayer.Type.ADJUST);
@@ -34,7 +36,7 @@ public class LocationObserver implements Observer<LocationData> {
     }
 
     public void play(double consumption) {
-        System.out.println("consumption: " + consumption);
+        consumption = expAverage(consumption);
         if (consumption < THRESHOLD_NORMAL) {
             sp_normal.play(0);
             sp_high.play(0);
@@ -44,16 +46,21 @@ public class LocationObserver implements Observer<LocationData> {
         } else if (consumption > THRESHOLD_EXTREME) {
             sp_high.play(1);
             sp_extreme.play(1);
-            System.out.println("ROARING THUNDER!");
         } else {
             double highconsumption_amplitude = (consumption - THRESHOLD_HIGH) / (THRESHOLD_EXTREME - THRESHOLD_HIGH);
-            System.out.println("blending: " + highconsumption_amplitude);
             sp_high.play(highconsumption_amplitude);
             sp_normal.play(1 - highconsumption_amplitude);
         }
     }
 
-    public static void setThresholds(double normal, double high, double extreme){
+    private double expAverage(double value) {
+        double newValue = lastValue + alpha * (value - lastValue);
+        lastValue = newValue;
+        System.out.println("new value:" + value + ", averaged new value: " + newValue);
+        return newValue;
+    }
+
+    public static void setThresholds(double normal, double high, double extreme) {
         THRESHOLD_NORMAL = normal;
         THRESHOLD_HIGH = high;
         THRESHOLD_EXTREME = extreme;
