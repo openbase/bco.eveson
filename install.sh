e4cafbcf171a6d8c562bd34cede4cc5191aa3f6a
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2017-09-13 20:21:57 +0200

    make install script quie

--- install.sh
+++ install.sh
@@ -2,9 +2,20 @@
 APP_NAME='eveson'
 clear &&
 echo "=== clean ${APP_NAME} ===" &&
-mvn clean $@ &&
+mvn clean --quiet $@ &&
 clear &&
-echo "=== install and deploy ${APP_NAME} ===" &&
-mvn install -DassembleDirectory=${prefix} -Daudio.resource.folder=${prefix}/share/audio/samples/eveson -Dconfig.folder=${prefix}/etc/eveson $@ &&
+echo "=== install ${APP_NAME} to ${prefix} ===" &&
+mvn install \
+        -DassembleDirectory=${prefix} \
+        -DskipTests=true \
+        -Dmaven.test.skip=true \
+        -Dlicense.skipAddThirdParty=true \
+        -Dlicense.skipUpdateProjectLicense=true \
+        -Dlicense.skipDownloadLicenses \
+        -Dlicense.skipCheckLicense=true \
+        -Dmaven.license.skip=true \
+        -Daudio.resource.folder=${prefix}/share/audio/samples/eveson \
+        -Dconfig.folder=${prefix}/etc/eveson \
+        --quiet $@ &&
 clear &&
 echo "=== ${APP_NAME} is successfully installed to ${prefix} ==="
