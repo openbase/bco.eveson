b70e0c8a822fd3651d8a41e7031faebd096e9b04
Divine Threepwood <divine@openbase.org>
2018-04-02 21:27:35 +0200

    implement usage of the unit pool instead initiating own unit remote. cleanup code.

--- src/main/java/org/openbase/bco/eveson/remotes/Remotes.java
+++ src/main/java/org/openbase/bco/eveson/remotes/Remotes.java
@@ -22,6 +22,7 @@ package org.openbase.bco.eveson.remotes;
  * #L%
  */
 
+import org.openbase.bco.dal.remote.unit.Units;
 import org.openbase.bco.eveson.EventPlayer;
 import org.openbase.bco.eveson.PowerTest;
 import java.util.ArrayList;
@@ -32,6 +33,7 @@ import org.openbase.bco.dal.remote.unit.MotionDetectorRemote;
 import org.openbase.bco.dal.remote.unit.location.LocationRemote;
 import org.openbase.bco.registry.location.lib.LocationRegistry;
 import org.openbase.bco.registry.location.remote.CachedLocationRegistryRemote;
+import org.openbase.bco.registry.remote.Registries;
 import org.openbase.bco.registry.unit.lib.UnitRegistry;
 import org.openbase.bco.registry.unit.remote.CachedUnitRegistryRemote;
 import org.openbase.jps.core.JPService;
@@ -42,6 +44,7 @@ import org.openbase.jul.exception.InitializationException;
 import org.openbase.jul.exception.InstantiationException;
 import rst.domotic.state.EnablingStateType.EnablingState;
 import rst.domotic.unit.UnitConfigType;
+import rst.domotic.unit.UnitConfigType.UnitConfig;
 import rst.domotic.unit.UnitTemplateType;
 
 /**
@@ -52,48 +55,29 @@ import rst.domotic.unit.UnitTemplateType;
  */
 public class Remotes {
 
-    UnitRegistry unitRegistry;
-    LocationRegistry locationRegistry;
-
-    public Remotes() throws InstantiationException, InterruptedException {
-        try {
-            CachedUnitRegistryRemote.waitForData();
-            CachedLocationRegistryRemote.waitForData();
-            this.unitRegistry = CachedUnitRegistryRemote.getRegistry();
-            this.locationRegistry = CachedLocationRegistryRemote.getRegistry();
-        } catch (CouldNotPerformException ex) {
-            Logger.getLogger(Remotes.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
     public void init() throws InterruptedException {
         try {
-            LocationRemote locationRemote = new LocationRemote();
-            locationRemote.init(locationRegistry.getRootLocationConfig());
-            locationRemote.activate();
             final LocationObserver locationObserver = new LocationObserver();
-            locationRemote.addDataObserver(locationObserver);
+            // connect observer to root location
+            Units.getUnit(Registries.getUnitRegistry(true).getRootLocationConfig(), false, Units.LOCATION).addDataObserver(locationObserver);
             if (JPService.getProperty(JPShowGUI.class).getValue()) {
                 new PowerTest(locationObserver).setVisible(true);
             }
-            List<UnitConfigType.UnitConfig> motionDetectors = unitRegistry.getUnitConfigs(UnitTemplateType.UnitTemplate.UnitType.MOTION_DETECTOR);
-            List<MotionDetectorRemote> motionDetectorRemotes = new ArrayList<>();
-
-            MotionDetectorRemote remote;
+            List<UnitConfig> motionDetectorConfigs = Registries.getUnitRegistry(true).getUnitConfigs(UnitTemplateType.UnitTemplate.UnitType.MOTION_DETECTOR);
 
             int i = 1;
             String id;
-            for (UnitConfigType.UnitConfig motionDetectorConfig : motionDetectors) {
 
+            for (final UnitConfig motionDetectorConfig : motionDetectorConfigs) {
+
+                // filter disabled motion detectors
                 if (motionDetectorConfig.getEnablingState().getValue() != EnablingState.State.ENABLED) {
                     continue;
                 }
-                remote = new MotionDetectorRemote();
-                remote.init(motionDetectorConfig);
-                remote.activate();
-                motionDetectorRemotes.add(remote);
+
+                final MotionDetectorRemote remote = Units.getUnit(motionDetectorConfig, false, Units.MOTION_DETECTOR);
+
                 id = motionDetectorConfig.getType().name() + "_" + i;
-//                System.out.println(id + ": Scope: " + motionSensorConfig.getScope().toString());
                 if (EventPlayer.getInstance().getScopeSampleMap().containsKey(id)) {
                     remote.addDataObserver(new MotionSensorObserver(id));
                 }
@@ -106,8 +90,6 @@ public class Remotes {
             Logger.getLogger(Remotes.class.getName()).log(Level.SEVERE, null, ex);
         } catch (JPNotAvailableException ex) {
             Logger.getLogger(Remotes.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            unitRegistry.shutdown();
         }
     }
 }
